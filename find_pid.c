1aab8e4f055f00015ecfa4dbf0488595884dd1e7
Myoungje Kim <mjei78@gmail.com>
2012-11-23 15:31:08 +0900

    Adjust tap size and white space
    Signed-off-by: Myoungje Kim <mjei78@gmail.com>

--- find_pid.c
+++ find_pid.c
@@ -12,67 +12,71 @@
 
 static void usage(char *progname)
 {
-    fprintf(stderr, "usage: %s <process_name> \n", progname);
+	fprintf(stderr, "usage: %s <process_name> \n", progname);
 }
 
 int main(int argc , char *argv[])
 {
 	DIR * dir;
 	struct dirent * dirent;
-    char sname[32], buf[512], process_name[32];
-    int fd, len, p_count=0;
-    unsigned pid;
-    char *app, *progname;
+	char sname[32], buf[512], process_name[32];
+	int fd, len, p_count=0;
+	unsigned pid;
+	char *app, *progname;
 
-    progname = strrchr(argv[0], '/');
-    progname = progname ? 1+progname : argv[0];
+	progname = strrchr(argv[0], '/');
+	progname = progname ? 1+progname : argv[0];
 
-    if(argc < 2) {
-        usage(progname);
-        return -1;
-    }
-
-    app = (char *)argv[1];
-
-	if (!(dir = opendir("/proc"))) {
-		perror("/proc directory could not be opened. ");
+	if(argc < 2) {
+		usage(progname);
 		return -1;
 	}
 
-	printf("\n Searching %s process ...\n\n", app);
-    printf(" Found ==> \n");
-    while ((dirent = readdir(dir))) {
-        snprintf(sname, sizeof(sname), "/proc/%s/stat", dirent->d_name);
-        if ((fd = open(sname, O_RDONLY)) < 0) {
-            continue;
-        }
-        
-        if ((len = read(fd, buf, sizeof(buf))) < 1 ) {
-            close(fd);
-            continue;
-        }
+	app = (char *)argv[1];
 
-        buf[len] = '\0';
-        close(fd);
-
-        // The start of the file looks like:
-        //  <pid> (<name>) R <parent pid>
-        if (sscanf(buf, "%u (%[^)]", &pid, process_name) != 2)
-            continue;
-        
-        //printf("pid: %u, process name: %s \n", pid, process_name);
-        if (strcmp(process_name, app) == 0) {
-            p_count ++;
-            printf("       pid: %u\n", pid);
-        }
+	if (!(dir = opendir("/proc"))) {
+		printf("/proc directory could not be opened. ");
+		return -1;
 	}
 
-    if(p_count)
-        printf("\n ... %d %s process found \n\n", p_count, app);
-    else
-        printf("       None\n\n ...%s not running or none exist \n\n", app);
+	printf("\n");
+	printf(" Searching %s process ...\n\n", app);
+	printf(" Found: \n");
+	
+	while ((dirent = readdir(dir))) {
+		snprintf(sname, sizeof(sname), "/proc/%s/stat", dirent->d_name);
+		if ((fd = open(sname, O_RDONLY)) < 0)
+			continue;
 
-    closedir(dir);
-    
-    return 0;
+		if ((len = read(fd, buf, sizeof(buf))) < 1 ) {
+			close(fd);
+			continue;
+		}
+
+		buf[len] = '\0';
+		close(fd);
+
+		// The start of the file looks like:
+		//  <pid> (<name>) R <parent pid>
+		if (sscanf(buf, "%u (%[^)]", &pid, process_name) != 2)
+			continue;
+
+		//printf("pid: %u, process name: %s \n", pid, process_name);
+		if (strcmp(process_name, app) == 0) {
+			p_count ++;
+			printf("       pid: %u \n", pid);
+		}
+	}
+
+	if(p_count) {
+		printf("\n");
+		printf(" ... %d %s process found \n\n", p_count, app);
+	}
+	else {
+		printf("       None \n\n");
+		printf(" ...%s not running or none exist \n\n", app);
+	}
+	
+	closedir(dir);
+	return 0;
 }
